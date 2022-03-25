4ffba1bc1eae4e3e01170dac6ad7a8666c8ee7c2
magnusagad <magnus.aga@dfind.no>
2022-03-25 16:28:19 +0100

    added right port

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-	await page.goto('localhost:9999');
+	await page.goto('localhost:3000');
 	const title = page.locator('h1');
 	await expect(title).toHaveText('Svelte tesseract.js');
 });
